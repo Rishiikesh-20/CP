07c51806b3eb4e12a27787c012156a653d6fadbf
Rishiikesh <rishiikeshsk@gmail.com>
2025-09-25 18:45:42 +0530

    done contest2146B problem

---
+++ contest2146B.java
@@ -0,0 +1,187 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+public class contest2146B{
+    static class Reader { 
+        final private int BUFFER_SIZE = 1 << 16; 
+        private DataInputStream din; 
+        private byte[] buffer; 
+        private int bufferPointer, bytesRead;
+        public Reader() { 
+            din = new DataInputStream(System.in); 
+            buffer = new byte[BUFFER_SIZE]; 
+            bufferPointer = bytesRead = 0; 
+        }
+        public Reader(String file_name) throws IOException { 
+            din = new DataInputStream(new FileInputStream(file_name)); 
+            buffer = new byte[BUFFER_SIZE]; 
+            bufferPointer = bytesRead = 0; 
+        }
+        public String readLine() throws IOException { 
+            byte[] buf = new byte[64]; // line length 
+            int cnt = 0, c; 
+            while ((c = read()) != -1) { 
+                if (c == '\n') 
+                    break; 
+                buf[cnt++] = (byte) c; 
+            } 
+            return new String(buf, 0, cnt); 
+        }
+        public int nextInt() throws IOException { 
+            int ret = 0; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+            do{ 
+                ret = ret * 10 + c - '0'; 
+            }  while ((c = read()) >= '0' && c <= '9');
+            if (neg) 
+                return -ret; 
+            return ret; 
+        }
+        public long nextLong() throws IOException { 
+            long ret = 0; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read(); 
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } 
+            while ((c = read()) >= '0' && c <= '9'); 
+            if (neg) 
+                return -ret; 
+            return ret; 
+        }
+        public double nextDouble() throws IOException { 
+            double ret = 0, div = 1; 
+            byte c = read(); 
+            while (c <= ' ') 
+                c = read(); 
+            boolean neg = (c == '-'); 
+            if (neg) 
+                c = read();
+            do { 
+                ret = ret * 10 + c - '0'; 
+            } 
+            while ((c = read()) >= '0' && c <= '9');
+            if (c == '.') { 
+                while ((c = read()) >= '0' && c <= '9') 
+                { 
+                    ret += (c - '0') / (div *= 10); 
+                } 
+            }
+            if (neg) 
+                return -ret; 
+            return ret; 
+        }
+        private void fillBuffer() throws IOException { 
+            bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE); 
+            if (bytesRead == -1) 
+                buffer[0] = -1; 
+        }
+        private byte read() throws IOException { 
+            if (bufferPointer == bytesRead) 
+                fillBuffer(); 
+            return buffer[bufferPointer++]; 
+        }
+        public void close() throws IOException { 
+            if (din == null) 
+                return; 
+            din.close(); 
+        } 
+    } 
+    static Reader sc=new Reader();
+    static BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String args[])throws IOException
+    {
+        /*  
+            1 to m 
+            n subsets
+            choose 0 to n subsets - Subset should contain atleast 
+            atleast 3 ways to choose 
+
+            1) 1st we need to check if all elements are present atleast once in any of the sets
+
+            2) Then take the sets which contains a value which other sets does not contain 
+
+            3) if you get those special sets , n-special>=2 , then it is satisfied
+
+        */
+
+        int t=inputInt();
+        testcases : while(t-->0){
+            int n=inputInt();
+            int m=inputInt();
+            ArrayList<HashSet<Integer>> arr=new ArrayList<>();
+            int[] numCount=new int[m+1];
+            int[] lastArr=new int[m+1];
+            for(int i=0;i<n;i++){
+                arr.add(new HashSet<>());
+                int l=inputInt();
+                for(int j=0;j<l;j++){
+                    int val=inputInt();
+                    numCount[val]++;
+                    lastArr[val]=i;
+                    arr.get(i).add(val);
+                } 
+            }
+            
+            // Case 1
+            for(int i=1;i<=m;i++){
+                if(numCount[i]==0){
+                    println("NO");
+                    continue testcases;
+                }
+            }
+            HashSet<Integer> specialSets=new HashSet<>();
+            // Case 2
+            for(int i=1;i<m+1;i++){
+                if(numCount[i]==1){
+                    specialSets.add(lastArr[i]);
+                }
+            }
+
+            if(n-specialSets.size()>=2){
+                println("YES");
+                continue testcases;
+            }
+            println("NO");
+        }
+        bw.flush();
+        bw.close();
+    }
+    public static int inputInt()throws IOException
+    {
+        return sc.nextInt();
+    }
+    public static long inputLong()throws IOException
+    {
+        return sc.nextLong();
+    }
+    public static double inputDouble()throws IOException
+    {
+        return sc.nextDouble();
+    }
+    public static String inputString()throws IOException
+    {
+        return sc.readLine();
+    }
+    public static void print(String a)throws IOException
+    {
+        bw.write(a);
+    }
+    public static void printSp(String a)throws IOException
+    {
+        bw.write(a+" ");
+    }
+    public static void println(String a)throws IOException
+    {
+        bw.write(a+"\n");
+    }
+}
