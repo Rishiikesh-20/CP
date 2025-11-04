007058f6f624ff8a16b774822750e7b16117cb4b
Rishiikesh <rishiikeshsk@gmail.com>
2025-11-04 16:31:42 +0530

    solved problem

---
+++ A_Beautiful_Average.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class A_Beautiful_Average{
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {}
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        FastScanner in = new FastScanner();
+
+        int t=in.nextInt();
+        while(t-->0){
+            int max=Integer.MIN_VALUE;
+            int n=in.nextInt();
+            for(int i=0;i<n;i++){
+                int val=in.nextInt();
+                if(val>max) max=val;
+            }
+            System.out.println(max);
+        }
+
+
+        out.close();
+    }
+}
