4df6e70ee6dcdeabddb515ad6bdf340ac3c67d50
Rishiikesh <rishiikeshsk@gmail.com>
2025-11-11 20:05:25 +0530

    solved problem

---
+++ A_Souvlaki_VS_Kalamaki.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class A_Souvlaki_VS_Kalamaki{
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {}
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        FastScanner in = new FastScanner();
+
+        int t=in.nextInt();
+        testcase: while(t-->0){
+            int n=in.nextInt();
+            int[] arr=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=in.nextInt();
+            }
+            Arrays.sort(arr);
+            for(int i=1;i<n-1;i+=2){
+                if(arr[i]!=arr[i+1]){
+                    System.out.println("NO");
+                    continue testcase;
+                }
+            }
+            System.out.println("YES");
+        }
+
+
+        out.close();
+    }
+}
