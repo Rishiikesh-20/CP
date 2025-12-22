66d92de52647427c7067cba5769bd75a08fd16e1
Rishiikesh <rishiikeshsk@gmail.com>
2025-12-22 09:10:50 +0530

    solved problem

---
+++ A_Pizza_Time.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class A_Pizza_Time{
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {}
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        FastScanner in = new FastScanner();
+
+        int t=in.nextInt();
+        testcase: while(t-->0){
+            int n=in.nextInt();
+            int ans=0;
+            while(n>=3){
+                ans=ans+(n/3);
+                n=(n/3)+(n%3);
+            }
+            System.out.println(ans);
+        }
+
+
+        out.close();
+    }
+}
