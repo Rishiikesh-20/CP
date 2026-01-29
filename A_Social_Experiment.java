68f4ce981e6a232cde15295e10560d1e82ed325d
Rishiikesh <rishiikeshsk@gmail.com>
2026-01-29 19:58:01 +0530

    solved problem

---
+++ A_Social_Experiment.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+public class A_Social_Experiment{
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {}
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        FastScanner in = new FastScanner();
+
+        int t=in.nextInt();
+        testcase: while(t-->0){
+            int n=in.nextInt();
+            if(n<=3){
+                System.out.println(n);
+                continue testcase;
+            }
+            if(n%2==0){
+                System.out.println(0);
+            }else{
+                System.out.println(1);
+            }  
+        }
+
+
+        out.close();
+    }
+}
