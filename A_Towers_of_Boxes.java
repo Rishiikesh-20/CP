82925cb5584d0141e2f59015d9f1a352a1b93e23
Rishiikesh <rishiikeshsk@gmail.com>
2026-02-28 10:07:36 +0530

    solved problem

---
+++ A_Towers_of_Boxes.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class A_Towers_of_Boxes{
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {}
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        FastScanner in = new FastScanner();
+
+        int t=in.nextInt();
+        testcase: while(t-->0){
+            int n=in.nextInt();
+            int m=in.nextInt();
+            int d=in.nextInt();
+
+            int x=(int)Math.floor((double)d/m);
+
+            System.out.println((int)Math.ceil((double)n/(x+1)));
+
+        }
+
+
+        out.close();
+    }
+}
