682ce22b4f041732505f4b89820fb22312c53d6a
Rishiikesh <rishiikeshsk@gmail.com>
2026-02-28 10:07:17 +0530

    solved problem

---
+++ A_Lawn_Mower.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class A_Lawn_Mower{
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {}
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        FastScanner in = new FastScanner();
+
+        int t=in.nextInt();
+        testcase: while(t-->0){
+            int n=in.nextInt();
+            int w=in.nextInt();
+
+            System.out.println(n-n/w);
+        }
+
+
+        out.close();
+    }
+}
