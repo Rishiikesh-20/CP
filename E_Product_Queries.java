5aec0499bdbfc7f0cc7a715983887b3bf0982218
Rishiikesh <rishiikeshsk@gmail.com>
2026-02-28 10:09:11 +0530

    solved problem

---
+++ E_Product_Queries.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class E_Product_Queries{
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {}
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        FastScanner in = new FastScanner();
+
+        int t=in.nextInt();
+        testcase: while(t-->0){
+            int n=in.nextInt();
+            int[] arr=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=in.nextInt();
+            }
+
+            HashSet<Integer> set=new HashSet<>();
+
+            for(int i=0;i<n;i++){
+                set.add(arr[i]);
+            }
+            for(int i=1;i<=n;i++){
+                if(set.contains(i)){
+                    System.out.println(1);
+                    continue;
+                }
+                while(n>1){
+                    
+                }
+            }
+        }
+
+
+        out.close();
+    }
+}
