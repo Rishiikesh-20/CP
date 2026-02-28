1b93799ef98fa6ababdc11e76238683ff675a5ed
Rishiikesh <rishiikeshsk@gmail.com>
2026-02-28 10:08:10 +0530

    solved problem

---
+++ C_1_XOR_Convenience_Easy_Version.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class C_1_XOR_Convenience_Easy_Version{
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {}
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        FastScanner in = new FastScanner();
+
+        int t=in.nextInt();
+        testcase: while(t-->0){
+            
+        }
+
+
+        out.close();
+    }
+}
