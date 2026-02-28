12ba9f09d9b618c74f61a27b811939ac00a196ea
Rishiikesh <rishiikeshsk@gmail.com>
2026-02-28 10:07:40 +0530

    solved problem

---
+++ A_Watermelon.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class A_Watermelon{
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {}
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        FastScanner in = new FastScanner();
+
+        int n=in.nextInt();
+        if(n%2!=0 || n==2){
+            System.out.println("NO");
+        }else{
+            System.out.println("YES");
+        }
+
+
+        out.close();
+    }
+}
