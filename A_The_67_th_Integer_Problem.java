4b4e8248366b1dea3bc2954fcee87bc87a427b4b
Rishiikesh <rishiikeshsk@gmail.com>
2026-04-08 20:05:31 +0530

    solved problem

---
+++ A_The_67_th_Integer_Problem.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class A_The_67_th_Integer_Problem{
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {}
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                return null;
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        PrintWriter out = new PrintWriter(System.out);
+        FastScanner in = new FastScanner();
+
+        int t=in.nextInt();
+        testcase: while(t-->0){
+            int n=in.nextInt();
+            if(n==67){
+                out.println(67);
+                continue;
+            }
+            out.println(n+1);
+        }
+
+
+        out.close();
+    }
+}
